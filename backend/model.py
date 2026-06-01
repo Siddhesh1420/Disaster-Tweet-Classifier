d626506d569908d441114816ff2f2d9d93cb6d09
Siddhesh1420 <siddheshdiptil@gmail.com>
2026-06-01 20:41:17 +0530

    Load model from Hugging Face

--- backend/model.py
+++ backend/model.py
@@ -1,5 +1,7 @@
 from transformers import pipeline
+print("Loading model from Hugging Face...")
 classifier=pipeline("text-classification",model="Sid1409/disaster-tweet-classifier")
+print("Model loaded successfully!")
 def predict(text:str):
     result=classifier(text)[0]
     label='Disaster' if result['label']=='LABEL_1' else 'Not Disaster'
