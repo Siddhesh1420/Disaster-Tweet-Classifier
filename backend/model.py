b4e1951fd02d226db040112289c024caa7c8afb6
Siddhesh1420 <siddheshdiptil@gmail.com>
2026-06-01 22:46:52 +0530

    Load model from Hugging Face

--- backend/model.py
+++ backend/model.py
@@ -1,6 +1,7 @@
 from transformers import pipeline
+import torch
 print("Loading model from Hugging Face...")
-classifier=pipeline("text-classification",model="Sid1409/disaster-tweet-classifier")
+classifier=pipeline("text-classification",model="Sid1409/disaster-tweet-classifier",device=-1,  dtype=torch.float32,truncation=True, max_length=128)
 print("Model loaded successfully!")
 def predict(text:str):
     result=classifier(text)[0]
